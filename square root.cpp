718143b976be0cf2e9ed3b7265c77b4785a4edfc
Lakshmi Nandan <127075476+Nandanroyals@users.noreply.github.com>
2023-05-17 21:18:05 +0530

    Add files via upload

---
+++ square root.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	float n;
+	printf("enter a float value:");
+	scanf("%f",&n);
+	printf("square root of float value 0f %6.2f is %6.2f",n,sqrt(n));
+
+}
